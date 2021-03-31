47b87909b752df57fbfebe4e6751772a0204fc63
Radha-982 <infy_1100417@outlook.com>
2021-03-31 16:50:47 +0530

    Code comments done

--- Bank.py
+++ Bank.py
@@ -1,78 +1,112 @@
+#Defining  a class with name Bank_Account
+class Bank_Account:
+#Defining a function with name __init__
+    def __init__(self):
+#Initialize variables like balance for performing functionality
+        self.balance = 0
+#Trigger Function 'print' with parameters
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
 
-class Bank_Account: 
-	def __init__(self): 
-		self.balance=0
-		print("Hello!!! Welcome to the Deposit & Withdrawal Machine") 
+#Defining a function with name deposit
+    def deposit(self):
+        amount = float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+#Trigger Function 'print' with parameters
+        print("\n Amount Deposited:", amount)
 
-	def deposit(self): 
-		amount=float(input("Enter amount to be Deposited: ")) 
-		self.balance += amount 
-		print("\n Amount Deposited:",amount) 
+#Defining a function with name withdraw
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+#Use conditional statement on parameters/check the options
+        if self.balance >= amount:
+            self.balance -= amount
+#Trigger Function 'print' with parameters
+            print("\n You Withdrew:", amount)
+        else:
+#Trigger Function 'print' with parameters
+            print("\n Insufficient balance ")
 
-	def withdraw(self): 
-		amount = float(input("Enter amount to be Withdrawn: ")) 
-		if self.balance>=amount: 
-			self.balance-=amount 
-			print("\n You Withdrew:", amount) 
-		else: 
-			print("\n Insufficient balance ") 
-
-	def display(self): 
-		print("\n Net Available Balance=",self.balance) 
+#Defining a function with name display
+    def display(self):
+#Trigger Function 'print' with parameters
+        print("\n Net Available Balance=", self.balance)
 
 
-s = Bank_Account() 
-s.deposit() 
-s.withdraw() 
-s.display() 
-
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
 
 start = 11
 end = 25
-for i in range(start,end+1):
-    if(i>1):
-        for j in range(2,i):
-            if(i%j==0):
+#Trigger Function 'range' with parameters
+#Repeat action till iterator completes
+for i in range(start, end + 1):
+#Use conditional statement on parameters/check the options
+    if (i > 1):
+#Trigger Function 'range' with parameters
+#Repeat action till iterator completes
+        for j in range(2, i):
+#Use conditional statement on parameters/check the options
+            if (i % j == 0):
                 break
         else:
+#Trigger Function 'print' with parameters
             print(i)
 
 
+#Defining  a class with name Bank_Account
+class Bank_Account:
+#Defining a function with name __init__
+    def __init__(self):
+#Initialize variables like balance for performing functionality
+        self.balance = 0
+#Trigger Function 'print' with parameters
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
 
-class Bank_Account: 
-	def __init__(self): 
-		self.balance=0
-		print("Hello!!! Welcome to the Deposit & Withdrawal Machine") 
+#Defining a function with name deposit
+    def deposit(self):
+        amount = float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+#Trigger Function 'print' with parameters
+        print("\n Amount Deposited:", amount)
 
-	def deposit(self): 
-		amount=float(input("Enter amount to be Deposited: ")) 
-		self.balance += amount 
-		print("\n Amount Deposited:",amount) 
+#Defining a function with name withdraw
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+#Use conditional statement on parameters/check the options
+        if self.balance >= amount:
+            self.balance -= amount
+#Trigger Function 'print' with parameters
+            print("\n You Withdrew:", amount)
+        else:
+#Trigger Function 'print' with parameters
+            print("\n Insufficient balance ")
 
-	def withdraw(self): 
-		amount = float(input("Enter amount to be Withdrawn: ")) 
-		if self.balance>=amount: 
-			self.balance-=amount 
-			print("\n You Withdrew:", amount) 
-		else: 
-			print("\n Insufficient balance ") 
-
-	def display(self): 
-		print("\n Net Available Balance=",self.balance) 
+#Defining a function with name display
+    def display(self):
+#Trigger Function 'print' with parameters
+        print("\n Net Available Balance=", self.balance)
 
 
-s = Bank_Account() 
-s.deposit() 
-s.withdraw() 
-s.display() 
-
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
 
 start = 11
 end = 25
-for i in range(start,end+1):
-    if(i>1):
-        for j in range(2,i):
-            if(i%j==0):
+#Trigger Function 'range' with parameters
+#Repeat action till iterator completes
+for i in range(start, end + 1):
+#Use conditional statement on parameters/check the options
+    if (i > 1):
+#Trigger Function 'range' with parameters
+#Repeat action till iterator completes
+        for j in range(2, i):
+#Use conditional statement on parameters/check the options
+            if (i % j == 0):
                 break
         else:
+#Trigger Function 'print' with parameters
             print(i)
--- Brahm.py
+++ Brahm.py
@@ -2,13 +2,19 @@
 # whether x is even or odd
 
 
+#Defining a function with name evenOdd
 def evenOdd(x):
-	if (x % 2 == 0):
-		print( "even")
-	else:
-		print( "odd")
+#Use conditional statement on parameters/check the options
+    if (x % 2 == 0):
+#Trigger Function 'print' with parameters
+        print("even")
+    else:
+#Trigger Function 'print' with parameters
+        print("odd")
 
 
 # Driver code to call the function
+#Trigger Function 'evenOdd' with parameters
 evenOdd(2)
+#Trigger Function 'evenOdd' with parameters
 evenOdd(3)
